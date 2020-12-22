cf22a609fa4331743ce4e3c76e86a273cfa117b5
Aftab Hussain <aftabhussainprojects@gmail.com>
2020-12-22 14:29:02 +0000

    Vimeo API call test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const clientId = process.env.REACT_APP_V_ID;
+const clientSecret = process.env.REACT_APP_V_SECRET;
+const accessToken = process.env.REACT_APP_V_TOKEN;
+
+let Vimeo = require("vimeo").Vimeo;
+let client = new Vimeo(clientId, clientSecret, accessToken);
+
+client.request(
+  {
+    method: "GET",
+    path: "/me/videos", //"/tutorial",
+  },
+  function (error, body, status_code, headers) {
+    if (error) {
+      console.log(error);
+    }
+
+    console.log(body);
+  }
+);
 
 function App() {
   return (
