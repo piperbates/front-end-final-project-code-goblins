10010019797828a10c4905696b89dd15ca428027
Aftab Hussain <aftabhussainprojects@gmail.com>
2021-01-05 15:17:46 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
-import {useEffect, useState} from "react"
+import { useEffect, useState } from "react";
 
 const clientId = process.env.REACT_APP_V_ID;
 const clientSecret = process.env.REACT_APP_V_SECRET;
@@ -10,10 +10,10 @@ let Vimeo = require("vimeo").Vimeo;
 let client = new Vimeo(clientId, clientSecret, accessToken);
 
 function App() {
-  const [data, setData] = useState({})
+  const [data, setData] = useState({});
   useEffect(() => {
-    async function getData() {
-      await client.request(
+    function getData() {
+      client.request(
         {
           method: "GET",
           path: "/me/videos", //"/tutorial",
@@ -22,25 +22,29 @@ function App() {
           if (error) {
             console.log(error);
           }
-      
-          console.log(body);
-          setData(body)
-          console.log(data)
+
+          //console.log(body);
+          setData({ ...body.data });
         }
-        );
+      );
     }
     getData();
   }, []);
-   
 
-function handleChange(e){
-  console.log(e.target.value)
-} 
+  console.log(data);
+  if (data.length > 0) {
+    console.log(data[4].name);
+    console.log(data[4].link);
+    console.log(data[4].pictures.sizes[3].link);
+  }
 
+  function handleChange(e) {
+    console.log(e.target.value);
+  }
 
   return (
     <div className="App">
-      <select onChange = {handleChange}>
+      <select onChange={handleChange}>
         {/* <option value={data[0].uri}>{data[0].name}</option>
         <option value={data[1].uri}>{data[1].name}</option>
         <option value={data[2].uri}>{data[2].name}</option>
