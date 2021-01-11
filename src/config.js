af0efbb7d9a5885d17a449455c0635aa2a7d51a5
Piper <bates.sarah42@gmail.com>
2021-01-11 19:04:45 +0000

    left in a slash bc of course

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const url = 'https://code-goblins-final-project.herokuapp.com/'
+const url = 'https://code-goblins-final-project.herokuapp.com'
 
 const config = {
   BACKEND_URL_SEARCH: `${url}/search/?`,
