18f713ac829f6277f9df61fccbb7d452c0bc5927
Piper <bates.sarah42@gmail.com>
2021-01-11 17:55:47 +0000

    deploying

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 const config = {
-  BACKEND_URL_SEARCH: `http://localhost:5000/search/?`,
-  BACKEND_URL_SEARCH_BY_ID: `http://localhost:5000/searchbyid/`,
-  BACKEND_URL_ADD_CONTENT: `http://localhost:5000/addcontent/`,
-  BACKEND_URL_VIMEO_GET_ALL_DATA: `http://localhost:5000/vimeo/allData/`,
-  BACKEND_URL_TAGS_GET_ALL_DATA: `http://localhost:5000/tags/`,
-  BACKEND_URL_TAGS_DELETE: `http://localhost:5000/tags/`,
-  BACKEND_URL_TAGS_ADD: `http://localhost:5000/tags/`,
-  BACKEND_URL_TAGS_UPDATE: `http://localhost:5000/tags/`,
+  BACKEND_URL_SEARCH: `https://code-goblins-final-project.herokuapp.com/search/?`,
+  BACKEND_URL_SEARCH_BY_ID: `https://code-goblins-final-project.herokuapp.com/searchbyid/`,
+  BACKEND_URL_ADD_CONTENT: `https://code-goblins-final-project.herokuapp.com/addcontent/`,
+  BACKEND_URL_VIMEO_GET_ALL_DATA: `https://code-goblins-final-project.herokuapp.com/vimeo/allData/`,
+  BACKEND_URL_TAGS_GET_ALL_DATA: `https://code-goblins-final-project.herokuapp.com/tags/`,
+  BACKEND_URL_TAGS_DELETE: `https://code-goblins-final-project.herokuapp.com/tags/`,
+  BACKEND_URL_TAGS_ADD: `https://code-goblins-final-project.herokuapp.com/tags/`,
+  BACKEND_URL_TAGS_UPDATE: `https://code-goblins-final-project.herokuapp.com/tags/`,
 };
 
 export default config;
